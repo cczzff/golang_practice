fd2086ae26e0cd89f9fa357d00cf0ef4fc99787c
cc <cong.zaifeng@myquant.cn>
2018-06-25 13:54:28 +0800

    简单的一个test

---
+++ test/auth_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"testing"
+	"fmt"
+	"google.golang.org/grpc"
+	"golang_practice/core"
+	"golang.org/x/net/context"
+)
+
+func TestAuthClient(t *testing.T) {
+	conn, err := grpc.Dial("localhost:8765", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Dial error: ", err)
+		return
+	}
+
+	cli := core_auth.NewAuthServiceClient(conn)
+
+	req := &core_auth.RegisterReq{}
+	res, err := cli.Register(context.Background(), req)
+	fmt.Println(res, err)
+}
